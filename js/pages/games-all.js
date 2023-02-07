f6f1ade1c2a04a62521796b36fedc8fc30efa1e3
Marte <marte.mk@hotmail.com>
2023-02-07 14:24:56 +0100

    Added search functionality and filtering

--- js/pages/games-all.js
+++ js/pages/games-all.js
@@ -2,25 +2,34 @@ import { fetchApiResults, parseGameRes } from "../util/api.js";
 import { createBoxError } from "../components/error.js";
 import { filterGames } from "../util/filter-games.js";
 
+const search = new URLSearchParams(window.location.search).get("search")?.toLowerCase();
+
 export const setupGamesAll = async () => {
   const gamesContainer = document.querySelector("#games-container");
   const main = document.querySelector("#main-content");
   const filter = document.querySelector("#filter");
 
   const parsedGames = [];
+
   try {
     const games = await fetchApiResults("/products", "?per_page=20");
     for (const game of games) {
       parsedGames.push(parseGameRes(game));
     }
 
+    let filteredGames = parsedGames;
+
     main.classList.remove("d-none");
     document.querySelector(".loader").classList.add("d-none");
 
-    filterGames(parsedGames, gamesContainer);
+    if (search) {
+      filteredGames = parsedGames.filter((game) => game.name.toLowerCase().startsWith(search));
+    }
+
+    filterGames(filteredGames, gamesContainer);
 
     filter.addEventListener("change", () => {
-      filterGames(parsedGames, gamesContainer);
+      filterGames(filteredGames, gamesContainer);
     });
   } catch (error) {
     console.log(error);
