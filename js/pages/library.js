c37825cb6de1bf4255065437ae1d5516281bb233
Marte <marte.mk@hotmail.com>
2023-02-09 19:43:23 +0100

    Made changes to libaray to only show one game just to mimmic the functionality of the page

--- js/pages/library.js
+++ js/pages/library.js
@@ -3,23 +3,15 @@ import { fetchApiResults, parseGameRes } from "../util/api.js";
 import { createBoxError } from "../components/error.js";
 
 const gamesContainer = document.querySelector("#games-container");
-
+const searchButton = document.querySelector("library-button");
+const searchInput = document.querySelector("library-search");
 export const setupLibrary = async () => {
   try {
-    const games = await fetchApiResults("/products", "?per_page=20");
+    const game = await parseGameRes(await fetchApiResults(`/products/75`));
     document.querySelector(".loader").classList.add("d-none");
-    const parsedGames = [];
-    for (const game of games) {
-      parsedGames.push(parseGameRes(game));
-    }
 
-    const ownedGames = parsedGames.splice(0, 3);
-
-    for (const game of ownedGames) {
-      console.log(game);
-      const card = createCard(game, true);
-      gamesContainer.appendChild(card);
-    }
+    let card = createCard(game, true);
+    gamesContainer.appendChild(card);
   } catch (error) {
     document.querySelector(".loader").classList.add("d-none");
 
